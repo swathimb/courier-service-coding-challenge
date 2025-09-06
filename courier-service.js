044ce3c3c8306b7598e9ca5675118db214f11cfd
Swathi <swathimb41@gmail.com>
2025-09-06 15:42:42 +0530

    challenge 2 WIP

--- courier-service.js
+++ courier-service.js
@@ -40,7 +40,7 @@ console.log(
 function getDiscount(deliveryCost, offerCode, distance, weight) {
   const offer = offers[offerCode];
   if (offer && offer.distance(distance) && offer.weight(weight)) {
-    return deliveryCost * offer.discount;
+    return parseFloat((deliveryCost * offer.discount).toFixed(2));
   }
   return 0;
 }
@@ -84,6 +84,7 @@ function handlepackageDetails(line) {
   const discount = getDiscount(deliveryCost, offerCode, distance, weight);
   const totalCost = deliveryCost - discount;
   packages.push({
+    id: currentLine + 1,
     pkgId,
     weight,
     distance,
@@ -122,6 +123,62 @@ function handleVehicleDetails(line) {
   rl.close();
 }
 
+function handleDeliveryCombinations(packages, maxWeight) {
+  let sortedByWeight = [...packages].sort((a, b) => b.weight - a.weight); // sort descending
+  let combinations = [];
+  while (sortedByWeight.length > 0) {
+    let combo = [];
+    let sum = 0;
+
+    for (let i = 0; i < sortedByWeight.length; i++) {
+      if (sum + sortedByWeight[i].weight <= maxWeight) {
+        sum += sortedByWeight[i].weight;
+        combo.push(sortedByWeight[i]);
+      }
+    }
+
+    if (combo.length === 0) break;
+
+    combinations.push(combo);
+    sortedByWeight = sortedByWeight.filter(n => !combo.includes(n));
+
+  }
+  // Sort combination:
+    combinations.sort((a, b) => {
+      if (b.length !== a.length) {
+        return b.length - a.length; // more elements first
+      }
+      if (a.length === 1 && b.length === 1) {
+        return b[0] - a[0]; // single-element combos: sort descending
+      }
+      return 0; // keep order otherwise
+    });
+
+    return combinations;
+}
+
+function calculateDeliveryTimes(deliveryCombinations, vehiclesInfo) {
+  let vehicles = new Array(vehiclesInfo.noVehicles).fill(0);
+  for(let i = 0; i < deliveryCombinations.length; i++) {
+    let nextVehicleIndex = vehicles.indexOf(Math.min(...vehicles));
+    let j =0;
+    let estimatedTime = [];
+    while(j < deliveryCombinations[i].length) {
+      const time = parseFloat((deliveryCombinations[i][j].distance / vehiclesInfo.speed).toFixed(2));
+      deliveryCombinations[i][j].estimatedDeliveryTime = (vehicles[nextVehicleIndex] + time);
+      estimatedTime.push(time);
+      j++;
+    }
+
+    let deliveryTime = Math.max(...estimatedTime);
+    if(i < deliveryCombinations.length + 1){
+      vehicles[nextVehicleIndex] = vehicles[nextVehicleIndex] + 2 * deliveryTime;
+    } else {
+      vehicles[nextVehicleIndex] += deliveryTime;
+    }
+  }
+}
+
 rl.on("line", (line) => {
   if (!baseCost) {
     // Base Delivery Cost and Number of Packages
@@ -136,7 +193,9 @@ rl.on("line", (line) => {
 });
 
 rl.on("close", () => {
-  console.log("\nAll inputs received successfully!");
-  console.log("Packages:", packages);
-  console.log("Vehicles Info:", vehiclesInfo);
+  const deliveryCombinations = handleDeliveryCombinations(packages, vehiclesInfo.maxWeight);
+  calculateDeliveryTimes(deliveryCombinations, vehiclesInfo);
+  const output = deliveryCombinations.flat().sort((a, b) => a.id - b.id);
+  console.log(output.map(pkg => `${pkg.pkgId} ${pkg.discount} ${pkg.totalCost} ${pkg.estimatedDeliveryTime}`).join("\n"));
+
 });
